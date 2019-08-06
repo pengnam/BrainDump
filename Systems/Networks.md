24b03ff3d4b105d532d5a935a6eb0750eac0445d
pengnam <seanngpengnam@gmail.com>
2019-08-06 16:34:56 +0800

    Update css path for writings

---
+++ Systems/Networks.md
@@ -0,0 +1,9 @@
+# Networks
+
+## URIs vs URLs
+
+URI: Uniform resource indicator
+
+URL: Uniform resource locator
+
+URL is a subset of URI. URI is the identifier of a resource and it can be a locator. 
--- utility/build.sh
+++ utility/build.sh
@@ -2,6 +2,7 @@ rm -rf ../docs
 mkdir ../docs
 mkdir ../docs/writings
 cp assets/* ../docs/
+cp assets/* ../docs/writings/
 
 # scp -r externalpages/* antonjr:~/assets/
 # scp -r assets/site.css antonjr:~/assets/
